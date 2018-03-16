e6093fd6760b59612e8aa985af34de687cf88194
javiercolombo <javier.colombo@hotmail.com.ar>
2018-03-16 11:18:40 -0400

    testing

--- js/audio.js
+++ js/audio.js
@@ -1,45 +1,70 @@
 (function() {
 
-	var playingAudio;
+	// var playingAudio;
 
-	var onEnd = function() {
-		playingAudio = null;
-	}
+	// var onEnd = function() {
+	// 	playingAudio = null;
+	// }
 
-	function play(index) {
+	// function play(index) {
 
-		if(playingAudio) playingAudio.pause();
+	// 	if(playingAudio) playingAudio.pause();
 
-		if (index == undefined) {
-			rnd = Math.floor(Math.random() * Math.floor(2));
-			index = rnd
-		}
+	// 	if (index == undefined) {
+	// 		rnd = Math.floor(Math.random() * Math.floor(2));
+	// 		index = rnd
+	// 	}
 
-		fn = 'audios/audio' + index + '.ogg'
-		console.log ("About to load " + fn)
+	// 	fn = 'audios/audio' + index + '.ogg'
+	// 	console.log ("About to load " + fn)
 
 
-		var audio = new Audio(fn);
-		audio.addEventListener('canplaythrough', function() { 
+	// 	var audio = new Audio(fn);
+	// 	audio.addEventListener('canplaythrough', function() { 
 		
-			audio.play();
+	// 		audio.play();
 
-			playingAudio = audio;
-			playingAudio.onended = onEnd;
+	// 		playingAudio = audio;
+	// 		playingAudio.onended = onEnd;
 
-		}, false);
+	// 	}, false);
 
 
-	}
+	// }
 
-	function pause(audio) {
-		audio.pause();
+	// function pause(audio) {
+	// 	audio.pause();
+	// }
+
+	// $('.beli-button > a').on('click', function() {
+	// 	console.log('clicking');
+	// 	$(this).find('button').css('color', 'red');
+	// 	play($(this).find('button').attr('audio'));		
+	// });
+
+
+
+
+
+	/**
+	* Test 2
+	*/
+
+	var audios = {};
+
+	function init() {
+		console.log('init');
+	    var sound = new Audio('audios/audio1.ogg');
+	    sound.load();
+	    audios['_1'] = sound;
 	}
 
 	$('.beli-button > a').on('click', function() {
-		console.log('clicking');
-		$(this).find('button').css('color', 'red');
-		play($(this).find('button').attr('audio'));		
+		audios['_1'].play();
 	});
+
+
+	$('#btn_load_sounds').on('click', init);
+
 
 })();
