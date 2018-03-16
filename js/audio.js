b5655a2bdc06e59e668d7beb898f0b42fb64441b
javiercolombo <javier.colombo@hotmail.com.ar>
2018-03-15 21:09:31 -0400

    audio stop

--- js/audio.js
+++ js/audio.js
@@ -1,8 +1,25 @@
 (function() {
 
+	var playingAudio;
+
+	var onEnd = function() {
+		playingAudio = null;
+	}
+
 	function play(index) {
+
+		if(playingAudio) playingAudio.pause();
+
 		var audio = new Audio('audios/audio'+ index +'.ogg');
 		audio.play();
+
+		playingAudio = audio;
+		playingAudio.onended = onEnd;
+
+	}
+
+	function pause(audio) {
+		audio.pause();
 	}
 
 	$('.beli-button button').on('click', function() {
