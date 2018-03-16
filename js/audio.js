2ee17826af08d5736e51d2a1c7a68680d186d45f
javiercolombo <javier.colombo@hotmail.com.ar>
2018-03-15 20:59:31 -0400

    se pasa listener a audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,4 +1,15 @@
-function play() {
-	var audio = new Audio('audios/audio1.ogg');
-	audio.play();
-}
+(function() {
+
+	function play(index) {
+		var audio = new Audio('audios/audio'+ index +'.ogg');
+		audio.play();
+	}
+
+	$('.beli-button button').on('click', function() {
+		play($(this).attr('audio'));		
+	});
+
+})();
+
+
+
