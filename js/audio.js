85f04b240a659ea64b5d7860d8e3ff0a12d7159b
Luke Pearson <lucasgp@gmail.com>
2018-03-17 17:20:02 -0400

    added 8 buttons

--- js/audio.js
+++ js/audio.js
@@ -6,16 +6,41 @@
 	});
 
 	function loadAllAudio() {
-
-		for(var i = 0; i < audios.length; i++) {
-			audios[i].src = 'audios/audio' + i + '.mp3';
-			audios[i].play();
-			audios[i].pause();
-		}
-
 		audios[0].src = 'audios/tagufiao.mp3';
 		audios[0].play();
 		audios[0].pause();
+
+		audios[1].src = 'audios/cantoDePuelco.mp3';
+		audios[1].play();
+		audios[1].pause();
+
+		audios[2].src = 'audios/cantoDePuelcoBicho.mp3';
+		audios[2].play();
+		audios[2].pause();
+
+		audios[3].src = 'audios/elStrogolEsReal.mp3';
+		audios[3].play();
+		audios[3].pause();
+
+		audios[4].src = 'audios/estupilo.mp3';
+		audios[4].play();
+		audios[4].pause();
+
+		audios[5].src = 'audios/fuckinMoron.mp3';
+		audios[5].play();
+		audios[5].pause();
+
+		audios[6].src = 'audios/jodidoToldo.mp3';
+		audios[6].play();
+		audios[6].pause();
+
+		audios[7].src = 'audios/estupilo2.mp3';
+		audios[7].play();
+		audios[7].pause();
+
+		audios[8].src = 'audios/unaGilada.mp3';
+		audios[8].play();
+		audios[8].pause();
 
 	}
 
